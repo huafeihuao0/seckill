6044d2c13c40c47cef3c248a112394e0ea1e7361
huafeihuao0 <yx16395175@163.com>
2018-03-31 05:30:16 +0800

    重构controller

--- src/main/java/org/seckill/web/SeckillController.java
+++ src/main/java/org/seckill/web/SeckillController.java
@@ -15,37 +15,45 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
-/**
- * @author cherry
- * @date 2017/12/11 19:50
- */
+
 @Controller
 @RequestMapping("/seckill")
-public class SeckillController {
-
+public class SeckillController
+{
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private SeckillService seckillService;
 
+    /***
+     *  秒杀商品列表
+     *      【/seckill/list】
+     * */
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String list(Model model){
-        List<Repo> list= seckillService.getSeckillList();
+    public String list(Model model)
+    {
+        List<Repo> list = seckillService.getSeckillList();
         model.addAttribute("list", list);
         return "list";
-        // /WEB-INF/jsp/list.jsp
     }
 
+    /***
+     *  商品详情
+     *      【/seckill/{productId}/detail】
+     * */
     @RequestMapping(value = "/{productId}/detail", method = RequestMethod.GET)
-    public String detail(@PathVariable("productId") Long productId, Model model){
-        if(productId == null){
+    public String detail(@PathVariable("productId") Long productId,
+                         Model model)
+    {
+        if (productId == null)
+        {
             return "redirect:/seckill/list";
         }
         Repo repo = seckillService.getById(productId);
-        if(repo == null){
+        if (repo == null)
+        {
             return "redirect:/seckill/list";
         }
         // 用于传向前端页面的变量seckill
@@ -54,51 +62,61 @@ public class SeckillController {
     }
 
     /**
-     * 返回的是一个json
+     * 商品展出
+     * 【/seckill/{productId}/exposure】
      */
-    @RequestMapping(value = "/{productId}/exposure", method = RequestMethod.POST,
-            produces = {"application/json;charset=utf-8"})
-    /**
-     *     告诉springmvc这是一个json输出
-      */
-    @ResponseBody
-    public SeckillResult<Exposure> exposure(@PathVariable("productId") Long productId){
+    @RequestMapping(value = "/{productId}/exposure",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=utf-8"})//响应头的内容类型
+    @ResponseBody   //返回值是json
+    public SeckillResult<Exposure> exposure(@PathVariable("productId") Long productId)
+    {
 
-        try{
+        try
+        {
             Exposure exposure = seckillService.exportSeckillUrl(productId);
             return new SeckillResult<Exposure>(true, exposure);
-        }catch(Exception e){
+        } catch (Exception e)
+        {
             logger.error(e.getMessage(), e);
-            return new SeckillResult<Exposure>(false,e.getMessage());
+            return new SeckillResult<Exposure>(false, e.getMessage());
         }
 
     }
 
-    @RequestMapping(value = "/{productId}/{md5}/execution", method = RequestMethod.POST,
+    /***
+     *  秒杀
+     * 【/seckill/{productId}/{md5}/execution】
+     * */
+    @RequestMapping(value = "/{productId}/{md5}/execution",
+            method = RequestMethod.POST,
             produces = {"application/json;charset=utf-8"})
-    /**
-     *     告诉springmvc这是一个json输出
-     */
     @ResponseBody
     public SeckillResult<SeckillExecution> execute(@PathVariable("productId") Long productId,
                                                    @CookieValue(value = "killPhone", required = false) Long userPhone,
-                                                   @PathVariable("md5") String md5){
-        if(userPhone == null) {
+                                                   @PathVariable("md5") String md5)
+    {
+        if (userPhone == null)
+        {
             return new SeckillResult<SeckillExecution>(false, "未注册");
         }
-        try{
+        try
+        {
             SeckillExecution seckillExecution = seckillService.executeSeckill(productId, userPhone, md5);
             return new SeckillResult<SeckillExecution>(true, seckillExecution);
 
-        }catch(SeckillRepeatException e){
+        } catch (SeckillRepeatException e)
+        {
             SeckillExecution seckillExecution = new SeckillExecution(productId, SeckillStateEnum.REPEAT_KILL);
             return new SeckillResult<SeckillExecution>(false, seckillExecution);
 
-        }catch(SeckillCloseException e){
+        } catch (SeckillCloseException e)
+        {
             SeckillExecution seckillExecution = new SeckillExecution(productId, SeckillStateEnum.END);
             return new SeckillResult<SeckillExecution>(false, seckillExecution);
 
-        } catch(Exception e){
+        } catch (Exception e)
+        {
             logger.error(e.getMessage(), e);
             SeckillExecution seckillExecution = new SeckillExecution(productId, SeckillStateEnum.INNER_ERROR);
             return new SeckillResult<SeckillExecution>(false, seckillExecution);
@@ -107,19 +125,16 @@ public class SeckillController {
 
     }
 
+    /***
+     *  时间
+     *      【/seckill/time/now】
+     * */
     @RequestMapping(value = "/time/now", method = RequestMethod.GET,
             produces = {"application/json;charset=utf-8"})
-    /**
-     *     告诉springmvc这是一个json输出
-     */
     @ResponseBody
-    public SeckillResult<Long> time(){
+    public SeckillResult<Long> time()
+    {
         Long now = System.currentTimeMillis();
         return new SeckillResult<Long>(true, now);
-
-
     }
-
-
-
 }
